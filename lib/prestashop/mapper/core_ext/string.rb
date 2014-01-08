b3f2ed96559c8e3bc31410a16902d8c8b5bb566f
JiriKolarik <jiri.kolarik@imin.cz>
2014-01-08 12:15:39 +0100

    Better restrictions

--- lib/prestashop/mapper/core_ext/string.rb
+++ lib/prestashop/mapper/core_ext/string.rb
@@ -1,20 +1,66 @@
 require 'open-uri'
 require 'sanitize'
 
+class Sanitize
+  module Config
+    IFRAMED = {
+      :elements => %w[
+        a abbr b bdo blockquote br caption cite code col colgroup dd del dfn dl
+        dt em figcaption figure h1 h2 h3 h4 h5 h6 hgroup i img ins kbd li mark
+        ol p pre q rp rt ruby s samp small strike strong sub sup table tbody td
+        tfoot th thead time tr u ul var wbr iframe
+        ],
+
+      :attributes => {
+        :all => ['dir', 'lang', 'title'],
+        'a' => ['href'],
+        'blockquote' => ['cite'],
+        'col' => ['span', 'width'],
+        'colgroup' => ['span', 'width'],
+        'del' => ['cite', 'datetime'],
+        'img' => ['align', 'alt', 'height', 'src', 'width'],
+        'ins' => ['cite', 'datetime'],
+        'ol' => ['start', 'reversed', 'type'],
+        'q' => ['cite'],
+        'table' => ['summary', 'width'],
+        'td' => ['abbr', 'axis', 'colspan', 'rowspan', 'width'],
+        'th' => ['abbr', 'axis', 'colspan', 'rowspan', 'scope', 'width'],
+        'time' => ['datetime', 'pubdate'],
+        'ul' => ['type'],
+        'iframe' => ['src']
+      },
+
+      :protocols => {
+        'a' => {'href' => ['ftp', 'http', 'https', 'mailto', :relative]},
+        'blockquote' => {'cite' => ['http', 'https', :relative]},
+        'del' => {'cite' => ['http', 'https', :relative]},
+        'img' => {'src' => ['http', 'https', :relative]},
+        'ins' => {'cite' => ['http', 'https', :relative]},
+        'q' => {'cite' => ['http', 'https', :relative]}
+      }
+    }
+  end
+end
+
 String.class_eval do 
-  def valid_url?
-    uri = URI.parse self
-    uri.kind_of?(URI::HTTP) or uri.kind_of?(URI::HTTPS)
-  rescue
-    false
+  def plain
+    self.clean.delete('<>;=#{}')
   end
 
-  def plain
-    self.restricted.delete('<>;=#{}')
+  def clean
+    Sanitize.clean self.unescape
   end
 
   def restricted
-    Sanitize.clean self.unescape
+    Sanitize.clean(self.unescape, Sanitize::Config::RESTRICTED)
+  end
+
+  def relaxed
+    Sanitize.clean(self.unescape, Sanitize::Config::RELAXED)
+  end
+
+  def iframed
+    Sanitize.clean(self.unescape, Sanitize::Config::IFRAMED)
   end
 
   def unescape
@@ -22,6 +68,6 @@ String.class_eval do
   end
 
   def html
-    Prestashop::Client.settings.html_enabled ? self.unescape : self.restricted
+    Prestashop::Client.settings.html_enabled ? self.iframed : self.relaxed
   end
 end
---
+++ test/test_mapper_core_ext.rb
@@ -0,0 +1,27 @@
+require_relative 'test_helper'
+
+describe String do 
+  it "should not contains <>;=\#{}" do
+    "<>st;r=i#n{g}".plain.must_equal "string"
+  end
+
+  it "should remove all html tags" do 
+    "I'm <a>Link</a>".clean.must_equal "I'm Link"
+  end
+
+  it "should keep some tags" do 
+    "I'm <a>Link</a> and <b>bold</b>".restricted.must_equal "I'm Link and <b>bold</b>"
+  end
+
+  it "should keep more tags" do 
+    "I'm <a>Link</a> and <b>bold</b>".relaxed.must_equal "I'm <a>Link</a> and <b>bold</b>"
+    "I'm <a>Link</a> and <b>bold</b><span onmouseover=\"_tipon(this)\" onmouseout=\"_tipoff()\">".relaxed.must_equal "I'm <a>Link</a> and <b>bold</b>"
+    "I'm <a>Link</a> and <b>bold</b><iframe src=\"http://www.w3schools.com\"></iframe>".relaxed.must_equal "I'm <a>Link</a> and <b>bold</b>"
+  end
+  
+  it "should keep more tags" do 
+    "I'm <a>Link</a> and <b>bold</b>".iframed.must_equal "I'm <a>Link</a> and <b>bold</b>"
+    "I'm <a>Link</a> and <b>bold</b><span onmouseover=\"_tipon(this)\" onmouseout=\"_tipoff()\">".iframed.must_equal "I'm <a>Link</a> and <b>bold</b>"
+    "I'm <a>Link</a> and <b>bold</b><iframe src=\"http://www.w3schools.com\"></iframe>".iframed.must_equal "I'm <a>Link</a> and <b>bold</b><iframe src=\"http://www.w3schools.com\"></iframe>"
+  end
+end
