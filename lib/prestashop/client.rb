1cacde64682ed93a2b981dc89b49783dee84c15e
JiriKolarik <jiri.kolarik@imin.cz>
2014-01-16 00:06:39 +0100

    Undefined supplier

--- lib/prestashop/client.rb
+++ lib/prestashop/client.rb
@@ -7,7 +7,7 @@ module Prestashop
     extend SingleForwardable
     def_delegators :current, :connection, :settings
     def_delegators :connection, :create, :read, :update, :delete, :check, :upload
-    def_delegators :settings, :country_iso_code, :lang_iso_code, :import_enabled, :update_enabled, :update_price, :update_stock,
+    def_delegators :settings, :country_iso_code, :lang_iso_code, :supplier, :import_enabled, :update_enabled, :update_price, :update_stock,
       :deactivate, :product_active, :product_available_for_order, :product_show_price, :vat_enabled, :available_now, :available_later,
       :delimiter, :html_enabled, :id_country, :id_language, :id_supplier, :taxes, :manufacturers_cache, :clear_manufacturers_cache,
       :categories_cache, :clear_categories_cache, :features_cache, :clear_features_cache, :feature_values_cache, :clear_feature_values_cache,
