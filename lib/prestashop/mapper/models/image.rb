438522bdb24658fb96ac4dfb277caaa8e0e7db79
JiriKolarik <jiri.kolarik@imin.cz>
2014-01-15 14:45:13 +0100

    Moving to self class

--- lib/prestashop/mapper/models/image.rb
+++ lib/prestashop/mapper/models/image.rb
@@ -5,28 +5,30 @@ module Prestashop
       resource :images
       model :image
 
-      def self.upload options = {}
-        if options[:file].kind_of? Array
-          images = []
-          options[:file].each do |file|
-            images << uploader(options[:resource], options[:id], file)
-          end
-          images.map{|i| i[:image][:id] } unless images.empty?
-        else
-          image = uploader(options[:resource], options[:id], options[:file])
-          image[:image][:id] if image
-        end if options[:file]
-      end
+      class << self
+        def upload options = {}
+          if options[:file].kind_of? Array
+            images = []
+            options[:file].each do |file|
+              images << uploader(options[:resource], options[:id], file)
+            end
+            images.map{|i| i[:image][:id] } unless images.empty?
+          else
+            image = uploader(options[:resource], options[:id], options[:file])
+            image[:image][:id] if image
+          end if options[:file]
+        end
 
-      def self.uploader resource, id, source
-        file = MiniMagick::Image.open(source)
-        Client.current.connection.upload 'images', resource, id, payload(file), file
-      rescue MiniMagick::Invalid
-        nil # It's not valid image
-      end
+        def uploader resource, id, source
+          file = MiniMagick::Image.open(source)
+          Client.current.connection.upload 'images', resource, id, payload(file), file
+        rescue MiniMagick::Invalid
+          nil # It's not valid image
+        end
 
-      def self.payload file
-        { image: Faraday::UploadIO.new(file.path, 'image') }
+        def payload file
+          { image: Faraday::UploadIO.new(file.path, 'image') }
+        end
       end
     end
   end
