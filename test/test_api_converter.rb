b54925d3c4022d87d0f7c4cc4c88a469f36feb9d
Jiri Kolarik <jiri.kolarik@imin.cz>
2014-04-23 23:43:51 +0200

    Hash cannot be used in Minitest 5

--- test/test_api_converter.rb
+++ test/test_api_converter.rb
@@ -3,15 +3,15 @@ require_relative 'test_helper'
 module Prestashop
   module Api
     describe Converter do
-      let(:hash) { { name: 'Steve', company: 'Apple' } }
+      let(:user) { { name: 'Steve', company: 'Apple' } }
       let(:xml) { '<prestashop xmlns:xlink="http://www.w3.org/1999/xlink"><user><name><![CDATA[Steve]]></name><company><![CDATA[Apple]]></company></user></prestashop>' }
 
       it "should generate xml" do 
-        Converter.build(:users, :user, hash).must_equal xml
+        Converter.build(:users, :user, user).must_equal xml
       end
 
       it "should generate xml with array" do 
-        Converter.parse(xml).must_equal({ user: hash })
+        Converter.parse(xml).must_equal({ user: user })
       end
     end
   end
