295acadb1bc426c2ec8d6c4a11a75467525e9559
Jiri Kolarik <jiri.kolarik@wereinhq.com>
2015-11-29 20:48:27 +0100

    Get `id_supplier` from args

--- lib/prestashop/mapper/models/product_supplier.rb
+++ lib/prestashop/mapper/models/product_supplier.rb
@@ -8,8 +8,7 @@ module Prestashop
       attr_reader :id_product, :id_product_attribute, :id_supplier, :product_supplier_reference, :product_supplier_price_te, :id_currency
 
       def initialize args = {}
-        @id_supplier = settings[:id_supplier]
-
+        @id_supplier          = args.fetch(:id_supplier)
         @id_product           = args.fetch(:id_product)
         @id_product_attribute = args.fetch(:id_product_attribute, 0)
         @id_currency          = args.fetch(:id_currency, 0)
